b8a8d7b8821e504aa651d2be76b49ba6110cf860
chientt <chientt@kaopiz.com>
2022-08-30 18:45:18 +0700

    init base

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,49 +1,49 @@
 // highlight
-import '../utils/highlight'
+import '../utils/highlight';
 
 // scroll bar
-import 'simplebar/src/simplebar.css'
+import 'simplebar/src/simplebar.css';
 
 // lazy image
-import 'react-lazy-load-image-component/src/effects/blur.css'
-import 'react-lazy-load-image-component/src/effects/opacity.css'
-import 'react-lazy-load-image-component/src/effects/black-and-white.css'
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 
 //
-import PropTypes from 'prop-types'
-import cookie from 'cookie'
+import PropTypes from 'prop-types';
+import cookie from 'cookie';
 // next
-import Head from 'next/head'
-import App from 'next/app'
+import Head from 'next/head';
+import App from 'next/app';
 //
-import { Provider as ReduxProvider } from 'react-redux'
-import { PersistGate } from 'redux-persist/lib/integration/react'
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 // @mui
-import AdapterDateFns from '@mui/lab/AdapterDateFns'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 // redux
-import { store, persistor } from '../redux/store'
+import { store, persistor } from '../redux/store';
 // utils
-import { getSettings } from '../utils/settings'
+import { getSettings } from '../utils/settings';
 // contexts
-import { SettingsProvider } from '../contexts/SettingsContext'
-import { CollapseDrawerProvider } from '../contexts/CollapseDrawerContext'
+import { SettingsProvider } from '../contexts/SettingsContext';
+import { CollapseDrawerProvider } from '../contexts/CollapseDrawerContext';
 // theme
-import ThemeProvider from '../theme'
+import ThemeProvider from '../theme';
 // components
-import Settings from '../components/settings'
-import { ChartStyle } from '../components/chart'
-import RtlLayout from '../components/RtlLayout'
-import ProgressBar from '../components/ProgressBar'
-import ThemeColorPresets from '../components/ThemeColorPresets'
-import NotistackProvider from '../components/NotistackProvider'
-import ThemeLocalization from '../components/ThemeLocalization'
-import MotionLazyContainer from '../components/animate/MotionLazyContainer'
+import Settings from '../components/settings';
+import { ChartStyle } from '../components/chart';
+import RtlLayout from '../components/RtlLayout';
+import ProgressBar from '../components/ProgressBar';
+import ThemeColorPresets from '../components/ThemeColorPresets';
+import NotistackProvider from '../components/NotistackProvider';
+import ThemeLocalization from '../components/ThemeLocalization';
+import MotionLazyContainer from '../components/animate/MotionLazyContainer';
 
 // Check our docs
 // https://docs-minimals.vercel.app/authentication/ts-version
 
-import { AuthProvider } from '../contexts/JWTContext'
+import { AuthProvider } from '../contexts/JWTContext';
 // import { AuthProvider } from '../contexts/Auth0Context';
 // import { AuthProvider } from '../contexts/FirebaseContext';
 // import { AuthProvider } from '../contexts/AwsCognitoContext';
@@ -53,63 +53,63 @@ import { AuthProvider } from '../contexts/JWTContext'
 MyApp.propTypes = {
   Component: PropTypes.func,
   pageProps: PropTypes.object,
-  settings: PropTypes.object
-}
+  settings: PropTypes.object,
+};
 
 export default function MyApp(props) {
-  const { Component, pageProps, settings } = props
+  const { Component, pageProps, settings } = props;
 
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
     <>
       <Head>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
       <AuthProvider>
         <ReduxProvider store={store}>
           <PersistGate loading={null} persistor={persistor}>
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <CollapseDrawerProvider>
-                <SettingsProvider defaultSettings={settings}>
-                  <ThemeProvider>
-                    <NotistackProvider>
-                      <MotionLazyContainer>
-                        <ThemeColorPresets>
-                          <ThemeLocalization>
-                            <RtlLayout>
-                              <ChartStyle />
-                              <Settings />
-                              <ProgressBar />
-                              {getLayout(<Component {...pageProps} />)}
-                            </RtlLayout>
-                          </ThemeLocalization>
-                        </ThemeColorPresets>
-                      </MotionLazyContainer>
-                    </NotistackProvider>
-                  </ThemeProvider>
-                </SettingsProvider>
-              </CollapseDrawerProvider>
-            </LocalizationProvider>
+            {/* <LocalizationProvider dateAdapter={AdapterDateFns}> */}
+            <CollapseDrawerProvider>
+              <SettingsProvider defaultSettings={settings}>
+                <ThemeProvider>
+                  <NotistackProvider>
+                    <MotionLazyContainer>
+                      <ThemeColorPresets>
+                        <ThemeLocalization>
+                          <RtlLayout>
+                            <ChartStyle />
+                            <Settings />
+                            <ProgressBar />
+                            {getLayout(<Component {...pageProps} />)}
+                          </RtlLayout>
+                        </ThemeLocalization>
+                      </ThemeColorPresets>
+                    </MotionLazyContainer>
+                  </NotistackProvider>
+                </ThemeProvider>
+              </SettingsProvider>
+            </CollapseDrawerProvider>
+            {/* </LocalizationProvider> */}
           </PersistGate>
         </ReduxProvider>
       </AuthProvider>
     </>
-  )
+  );
 }
 
 // ----------------------------------------------------------------------
 
 MyApp.getInitialProps = async (context) => {
-  const appProps = await App.getInitialProps(context)
+  const appProps = await App.getInitialProps(context);
 
-  const cookies = cookie.parse(context.ctx.req ? context.ctx.req.headers.cookie || '' : document.cookie)
+  const cookies = cookie.parse(context.ctx.req ? context.ctx.req.headers.cookie || '' : document.cookie);
 
-  const settings = getSettings(cookies)
+  const settings = getSettings(cookies);
 
   return {
     ...appProps,
-    settings
-  }
-}
+    settings,
+  };
+};
